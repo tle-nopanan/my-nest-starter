3b404e60db29eec5e366d56f786672eb4df6bf89
tle-nopanan <P.nopanan@kkumail.com>
2024-05-10 04:00:52 +0700

    docker-compose up -d

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './products/products.controller';
-import { ProductsService } from './products/products.service';
-import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
