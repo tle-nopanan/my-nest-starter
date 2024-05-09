cf258019d3b09dd16aaa9ab509763470418e4658
tle-nopanan <P.nopanan@kkumail.com>
2024-05-10 04:04:43 +0700

    nest g resource products

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       `mongodb://root:example@localhost:27017/mike?authSource=admin`,
     ),
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
